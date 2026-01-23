22ae3b7fc7bc122b0ebe9eec0b693b19e160e25b
DavidGillettSCCWRP <davidg@sccwrp.org>
2026-01-23 09:08:40 -0800

    rearranging files and adding new reference data  based on djg alternate SQO approach

---
+++ R/alt - Generic SQO BLOE calculator.R
@@ -0,0 +1,228 @@
+#Function designed to input benthic data for SQO calculation, calculate SQO Benthic Line of Evidence (BLOE), calculate US M-AMBI,
+#and produce both interim and final output files
+
+#This function requires that you have the following packages installed on your machine:
+#       1. tidyverse
+#       2. naniar
+#       3. openxlsx
+#       4. vegan
+#
+# in your r environments you can install packages with:  install.packages(".......")
+
+##################################################################################################################################
+# Instructions for use
+# The function has 3 required inputs:
+#      1. file_id - this is a quoted string for you to identify the data the BRI scores are associated with
+#           e.g., "Bight 23" or "2024 San Diego Bay" - this will be used to name all of the output files
+#      2. infauna_path - a quoted string detailing the name and location of the .csv file with infauna abundance
+#         data. Remember with path names, that R use forward slashes "/" not the normal backslash "\" that windows typically uses.
+#         All column names should be lower case. The expectation is that the file will have, at a minimum, a column for each of:
+#           stationid - unique identifier for that station, preferably formatted as character/text value
+#           sampledate - the date on which the sample was collected, must be in a mm/dd/yyyy format
+#           replicate - a number identifying the replicate infauna sample collected from the specified station on the specified date
+#           taxon - character string identifying the organism. naming conventions should follow SCAMIT edition 12
+#           abundance - a numeric value indicating the number of individuals counted for the specified taxon from the sample
+#           exclude - Yes or No value, indicating if the taxon name is ambiguous relative to other taxa in the sample
+#           depth - station depth in meters
+#           latitude - station latitude in decimal degrees
+#           longitude - station longitude in decimal degrees (negative values for west longitudes)
+#           salinity - bottom salinity at time of sample collection in PSU
+#       3. output_path - a quoted string detailing the location where you want the output files to be saved. Remember to use "/" not "\"
+###########################################################  ########################################################################
+### IMPORTANT: this "alt" version of the code transforms modern taxonomy to ed5 taxonomy to sidestep DJG's hypothesized diversity/richness problem arising from 
+###             the improving taxonomy of modern times. the indices are scaled to a certain level of diversity and fully adopting new, more specios species
+###             lists will alter the index scores to a degree. This version can be compared to a modern version to test this effect
+
+
+alt.SQO_BLOE.generic<-function(file_id, infauna_path, output_path)
+{
+  #loading required packages and the individual functions to calculate each of the individual SQO benthic indices
+  require(tidyverse)
+  require(naniar)
+  require(openxlsx)
+  require(vegan)
+  source("alternate species list approach/alt SQO BRI - generic.R")
+  source("alternate species list approach/alt IBI - generic.R")
+  source("alternate species list approach/alt RBI - generic.R")
+  source("alternate species list approach/alt RIVPACS - generic.R")
+  source("alternate species list approach/alt MAMBI - generic.R")
+  load("Reference Files/ed14_to_SQO.RData")#loading in files to convert from Ed14 taxonomy to SQO compatible taxonomy
+
+  print(paste("saving files to ", output_path, sep=""))
+
+  #cleaning up the infauna data to be analyzed
+  #standardizing the nomenclature of ommitted salinity or depth data to NA, so that it can be dealt with later
+  BenthicData<-read.csv(infauna_path) %>%
+  replace_with_na(., list(salinity=c(-88, -99), depth=c(-88, -99))) %>%
+    mutate(sampledate=mdy(sampledate))
+  
+#### retrofitting modern taxonomy back and taking daughter taxa and combining them up where appropriate ####
+  #ed14.rollups<-read.csv("alternate species list approach/SQO-ed14 roll up taxa.csv")
+  #SoCal.SQO.ed14.link<-read.csv("alternate species list approach/Bight 23 SQO Appendix - djg.csv")
+  #ed.14.complex<-read.csv("alternate species list approach/SQO-ed 14 complex change taxa.csv")
+
+  #Step 1 is to take one to one changes and swap the new names back to their old version 
+  one.to.one<-SoCal.SQO.ed14.link %>% 
+    select(Original.SQO.Taxon, Ed.14.Taxon, change, type) %>% 
+    filter(type%in%c("one-to-one", "spelling", "worms", "removed"))
+  
+  BenthicData.0<-BenthicData %>% 
+    left_join(., one.to.one, by=c("taxon"="Ed.14.Taxon")) %>% 
+    relocate(Original.SQO.Taxon, .before = taxon) %>% 
+    mutate(taxon.2=if_else(is.na(Original.SQO.Taxon), taxon, Original.SQO.Taxon),
+           change_type=if_else(is.na(type), "",type),
+           taxa_changed=if_else(is.na(Original.SQO.Taxon),"",taxon)) %>% 
+    
+    select(-taxon, -type, -change, -Original.SQO.Taxon)
+
+    
+  
+  
+  #Step two is to combine daughter taxa to the higher taxonomic level the indices recognize  
+  BenthicData.1<-BenthicData.0 %>% 
+    relocate(abundance,taxon.2, .before = stationid) %>% 
+    left_join(., ed14.rollups, by=c("taxon.2"="ed.14_daughters")) %>%
+    mutate(taxon.3=if_else(is.na(sqo.name), taxon.2, sqo.name),
+           rolled_up=if_else(is.na(sqo.name), "no", "yes"), .before = taxon.2) %>% 
+    #mutate(flag=if_else(taxon==sqo.name, 1,0), .before=abundance) %>% 
+    select(-sqo.name) %>% 
+    group_by(stationid, replicate, sampledate, taxon.3) %>%  
+    mutate(abundance.2=sum(abundance),
+           #when rolling up taxa we may be combining taxa w/ different exclude codes into one. To keep the most the taxa richness/exclude coding
+           # if any of the combined taxa has a No (i.e., it is unique), we give that prescedance to maintain the richness
+           all.excludes=str_flatten_comma(exclude),
+           exclude.prob.flag=case_when(str_detect(all.excludes, "No, Yes")~1,
+                                       str_detect(all.excludes, "Yes, No")~1,
+                                       TRUE~0),
+           exclude.2=case_when(rolled_up=="yes" & exclude.prob.flag==1 ~ "No",
+                               TRUE~exclude),
+           .before=abundance) %>% 
+    ungroup() %>% 
+    mutate(change_type.2=if_else(rolled_up=="yes", paste("rolled up to ",join_level, sep="" ),change_type),
+           taxa_changed.2=if_else(rolled_up=="yes", taxon.2, taxa_changed), 
+           .before = stationid) %>% 
+    select(-all.excludes, -exclude.prob.flag, -exclude) %>% 
+    rename(exclude=exclude.2)
+    
+  
+## Step 3 is to deal with complex changes where a single ed 14 taxon used to be multiple taxa on the SQO lu list
+## The taxa to choose among the old SQO names was prioritized to select a taxon that had a tolerance score, sensitivity designation, etc vesus one that didn't
+  ed.14.complex.2<-ed.14.complex %>% 
+    filter(Priority=="yes")
+  
+  BenthicData.2<-BenthicData.1 %>% 
+    left_join(., ed.14.complex.2, by=c("taxon.3"="Ed.14.Taxon")) %>% 
+    mutate(taxon.4=case_when(Priority=="yes"~ Original.SQO.Taxon,
+                             TRUE~taxon.3), .before = taxon.3) %>% 
+    mutate(change_type.3=case_when(Priority=="yes"~"complex",
+                                   TRUE~change_type.2), .before = change_type.2)
+  
+#Step 4 is to create an interim output file detailing all of the changes made to the submitted data
+  
+    taxa.changes<-BenthicData.2 %>%
+    group_by(stationid, replicate, sampledate, taxon.4, change_type.3) %>%
+  mutate(
+         taxa_changed.3= str_flatten_comma(taxa_changed.2),
+         .before=abundance) %>%
+    select(stationid, sampledate, replicate, taxon_used=taxon.4, taxon_submitted=taxon.2, changed_taxa=taxa_changed.3,
+           abundance_used=abundance.2,
+           abundance_submitted=abundance, type_of_change=change_type.3)
+
+
+  write.csv(taxa.changes, paste(output_path, "/interim data prep file - ed14 to SQO LU list taxa changes.csv", sep=""), row.names = FALSE)
+    
+# Step 5 is to create final file to run through the different indices.
+#Modern names have been changed to older, SQO-valid names and daughter taxa have been collapsed to the appropriate higher level
+#Where taxa were collapsed, their abundances were summed
+  
+  BenthicData.3<-BenthicData.2 %>% 
+   select(-abundance, -taxon.2, -taxon.3, -rolled_up, -change, -change_type, -change_type.2, -change_type.3,
+          -taxa_changed, -taxa_changed.2, -join_level, -Original.SQO.Taxon, -change, -type, -Priority) %>% 
+   distinct() %>% 
+   rename(taxon=taxon.4, abundance=abundance.2) %>% 
+   relocate(taxon, abundance, .after=sampledate)
+    
+    
+
+  #### running each of the individual benthic indices ####
+
+  bri.scores.x<-alt.SQO.BRI.generic(BenthicData.3, output_path, file_id)
+
+  ibi.scores.x<-alt.IBI.generic(BenthicData.3, output_path, file_id) #### fix ibi and rbi to drop non-essential station data before index calculations
+
+  rbi.scores.x<-alt.RBI.generic(BenthicData.3, output_path, file_id)
+
+  rivpacs.scores.x<-alt.RIVPACS.generic(BenthicData.3, output_path, file_id)
+  
+  mambi.scores.x<-alt.MAMBI.generic(BenthicData, EG_Ref_values = NULL, EG_Scheme = "Hybrid", output_path, file_id)
+  #Note that the MAMBI script uses modern taxonomy, as it can be updated unlike the SQO calculations
+  
+  
+
+  # aggregating all of the individual index scores so they can be reviewed by the user and used to calculate the BLOE score
+  all.sqo.scores.x<-bind_rows(bri.scores.x, ibi.scores.x, rbi.scores.x, rivpacs.scores.x) %>% 
+    arrange(., stationid, sampledate, replicate, index)
+
+
+  write.csv(all.sqo.scores.x, file=paste(output_path, "/", file_id, " SQO BLOE interim - all benthic indices scores.csv", sep=""), row.names = FALSE)
+
+
+  BLOE.scores.x<-all.sqo.scores.x %>%
+
+    group_by(stationid, sampledate, replicate) %>%
+    #Integrating the four separate indices by calculating the median of the scores and rounding up to the next integer
+    mutate(BLOE_score=ceiling(median(condition_category_score, na.rm=TRUE)),
+              Note=str_flatten(note, collapse=",")) %>%
+    ungroup() %>%
+    pivot_wider(id_cols=c(-score, -condition_category,  -note),
+                names_from = index, values_from = condition_category_score) %>%
+    # The notes column provides the user some context about the scores. Particularly about the appropriateness of the BLOE indices relative to salinity at time of sampling
+    # It will also carry over any notes generated during calculation of the individual indices
+    mutate(notes=case_when(is.na(salinity)&is.na(Note)~"Salinity value unknown-confirm salinity >=27 PSU for BLOE to be accurate.",
+                           is.na(salinity) & !is.na(Note)~paste("Salinity value unknown-confirm salinity >=27 PSU for BLOE to be accurate.", Note, sep="; "),
+                           salinity<27 & is.na(Note)~"Caution, salinity value <27 PSU - BLOE may not be accurate. Consider M-AMBI",
+                           salinity<27 & !is.na(Note)~paste("Caution, salinity value <27 PSU - BLOE may not be accurate. Consider M-AMBI", Note, sep="; "),
+                           salinity>=27 & !is.na(Note)~Note,
+                           salinity>=27 & is.na(Note)~"None",
+                           TRUE~"bob"),
+           BLOE_category=case_when(BLOE_score==1~"Reference",
+                                    BLOE_score==2~"Low Disturbance",
+                                    BLOE_score==3~"Moderate Disturbance",
+                                    BLOE_score==4~"High Disturbance")
+           ) %>%
+    relocate(stationid, sampledate, replicate, BLOE_score, BLOE_category, BRI_cond=BRI, IBI_cond=IBI, RBI_cond=RBI, Rivpacs_cond=Rivpacs) %>%
+    select(-Note)
+  
+  mambi.scores.sqoformat<-mambi.scores.x %>% #adjusting MAMBI output table to match the SQO BLOE indices format
+      mutate(MAMBI_cond=case_when(SQO_mambi_condition=="Reference"~1,
+                                SQO_mambi_condition=="Low Disturbance"~2,
+                                SQO_mambi_condition=="Moderate Disturbance"~3,
+                                SQO_mambi_condition=="High Disturbance"~4,
+                                TRUE~NA), .after=SQO_mambi_condition)
+  BLOE.scores.w.MAMBI<-BLOE.scores.x %>% 
+    left_join(., select(mambi.scores.sqoformat, stationid, sampledate, replicate, SQO_mambi_condition, MAMBI_cond, note ), by=c("stationid", "sampledate", "replicate")) %>% 
+    mutate(notes=case_when(note=="None"~notes,
+                             is.na(note)~notes,
+                             TRUE~paste(notes, note, sep="; "))) %>% 
+    select(-note) %>% 
+    rename(MAMBI_SQO_Cat=SQO_mambi_condition) %>% 
+    relocate(MAMBI_SQO_Cat, MAMBI_cond, .after=Rivpacs_cond)
+  
+  write.csv(BLOE.scores.x, file=paste(output_path, "/", file_id, " SQO integrated BLOE scores.csv", sep=""), row.names = FALSE)
+  
+  write.csv(BLOE.scores.w.MAMBI, file=paste(output_path, "/", file_id, " SQO integrated BLOE scores plus M-AMBI scores.csv", sep=""), row.names = FALSE)
+
+  #exporting everything into a single excel workbook with the BLOE scores, but also a tab for each individual index
+
+  #define sheet names for each data frame
+  # as M-AMBI is not included in the calculation of the traditional BLOE it is provided on a separate tab to avoid confusion. 
+  
+  dataset_names <- list("sqo_bloe"=BLOE.scores.x, "sqo_bri"=bri.scores.x, "ibi"=ibi.scores.x, "rbi"=rbi.scores.x, "rivpacs"=rivpacs.scores.x, 
+                        "sqo_bloe_w_mambi" =BLOE.scores.w.MAMBI, "mambi"=mambi.scores.sqoformat)
+
+  #export each data frame to separate sheets in same Excel file
+  write.xlsx(dataset_names, file = paste(output_path, "/", file_id, " SQO BLOE output summary.xlsx", sep=""))
+
+return(BLOE.scores.w.MAMBI)
+
+}
---
+++ R/alternate approach/alt IBI - generic.R
@@ -0,0 +1,201 @@
+# Compute the Index of Biotic Integrity (IBI) and IBI condition category.
+#
+
+#   The IBI is a a multi-metric index that compares the values of four different metrics to the ranges expected under 
+#   reference conditions. The score increases by one for each metric that is outside of the reference range. The four
+#   metrics are:
+#
+#   (1) the total number of taxa - measure of biodiversity 
+#   (2) the total number of mollusc taxa - measure of sensitivity to eutrophication and potentially invasive taxa
+#   (3) the abundance of Notomastus sp. - measure of the presence of organic matter indicative taxa
+#   (4) the number of sensitive taxa. - measure of the presence of pollution sensitive taxa designated by Thompson and Lowe, 2004
+#
+#   Details on the specifics of the calculation of the index can be found in Bay et al. 2021. Sediment Quality Assessment Techincal Support Manual. SCCWRP
+#      Technical Report 777
+#    Details on validation of the index can be found in Ranasinghe et al. 2009 Calibration and evaluation of five indicators of benthic community condition
+#       in two California bay and estuary habitats. Marine Pollution Bulletin 59:5-13
+#   Background concepts of the index can be found in Thompson and Lowe 2004. Assessment of macrobenthos response to sediment contamination in the
+#       San Francisco Estuary, California USA. Environmental Toxicology and Chemistry 23:2178-2187
+#   
+#       NOTE: This code is designed to be run in the Bight Program's Generic SQO BLOE calculator wrapper function. However, the function can
+#             be run independently but the user must load the following into their R environment:
+#                 1. file_id - this is a quoted string for you to identify the data the IBI scores are associated with
+#                   e.g., "Bight 23" or "2024 San Diego Bay" - this will be used to name all of the output files
+#                 2. output_path - a quoted string detailing the location where you want the output files to be saved. Remember to use "/" not "\"
+#                 3. BenthicData - a data frame containing the benthic data and the station information, detailed above
+#
+# This function will produce a csv file of final IBI scores for each sample, as well as csv files of interim tables detailing the taxa in the
+#     submitted data and how they are classified by the index, as well as, the benthic data classified into IBI metrics.
+
+alt.IBI.generic <- function(BenthicData, output_path, file_id)
+  
+{
+  require(tidyverse)
+  require(naniar)
+  load("Reference Files/SoCal SQO LU.RData")#not using new names. using alt approach of rolling names back then calculating using original LU list
+
+  #create an empty dataframe to populate with IBI scores
+  ibi.out.null<-tibble(stationid="dummy",
+                       replicate=NaN,
+                       sampledate=ymd("2000/01/1"),
+                       index="IBI",
+                       score=NaN,
+                       condition_category=NA,
+                       condition_category_score=NA,
+                       note=NA)
+  #in case a sample had no animals (e.g., taxon=NoOrganismsPresent), we force it into the High Disturbance category.
+  #the calculator would not be able to process that sample and would drop it, so we deal with it apriori
+  defaunated<-BenthicData %>%
+    filter(taxon=="NoOrganismsPresent") %>%
+    mutate(index="IBI",score=NaN, condition_category="High Disturbance", condition_category_score=4, note="Defaunated Sample") %>%
+    select(stationid, sampledate, replicate, index, score, condition_category, condition_category_score, note)
+
+  # Prepare the given data frame so that we can compute the IBI score and categories
+  ibi_data <- BenthicData %>%
+    filter(exclude!="Yes") %>%
+    left_join(orig.socal_sqo, by = c("taxon"="TaxonName")) %>%
+    
+    filter(taxon!="NoOrganismsPresent")
+
+  #Export data so the user knows what is going to used in subsequent calculations
+  
+  ibi_data.review<-ibi_data %>% 
+    mutate(Notomastus_flag=case_when(str_detect(taxon,"Notomastus")~ 1, 
+                                     TRUE~0)) %>% 
+    select(stationid, sampledate, replicate, taxon, abundance, exclude, Mollusc, IBISensitive, Notomastus_flag)
+    
+  write.csv(ibi_data.review, file = paste(output_path, "/", file_id, " SQO IBI interim 1 - data to be analyzed with SQO designations assigned.csv", sep=""),
+            row.names = FALSE)
+
+
+
+  # Calculate taxa richness
+  ibi1 <- ibi_data %>%
+     
+    mutate(rich_flag=case_when(Phylum==""~0,
+                               is.na(Phylum)~0,
+                               TRUE~1)) %>%
+    group_by(stationid, sampledate, replicate) %>%
+    summarise(NumOfTaxa =length(taxon))
+
+ 
+  # Calculate mollusc taxa richness
+  ibi2 <- ibi_data %>%
+    mutate(flag=(case_when(Mollusc=="Mollusc"~1, 
+                           TRUE~0))) %>%
+    group_by(stationid, sampledate, replicate) %>%
+    summarise(NumOfMolluscTaxa=sum(flag)) %>%
+    ungroup()
+
+
+  # calculate Notomastus spp. abundance
+  ibi3 <- ibi_data %>%
+    mutate(flag=case_when(str_detect(taxon,"Notomastus")~ abundance,
+                          TRUE~0)) %>%
+    group_by(stationid, sampledate, replicate) %>%
+    summarise(NotomastusAbun = sum(flag)) %>%
+    ungroup()
+
+
+  # Calculate % Sensitive Taxa
+  ibi4 <- ibi_data %>%
+    mutate(flag=case_when(IBISensitive=="S"~ 1, 
+                          TRUE~0) )%>%
+    group_by(stationid, sampledate, replicate) %>%
+    summarise(sensitive_S=sum(flag)) %>%
+    ungroup() %>%
+    left_join(ibi1, by=c("stationid", "sampledate", "replicate")) %>%
+    mutate(PctSensTaxa=(sensitive_S/NumOfTaxa)*100) %>%
+    select(stationid, sampledate, replicate, PctSensTaxa)
+
+ 
+  ### Reference ranges for IBI metrics in Southern California Marine Bays against which sample values are compared
+  ### [ Table 4.19 CASQO Technical Manual 3rd edition 2021 - page 68 ]
+  ibi_ref_ranges_table <- data.frame(ref_low = c(13, 2, 0, 19),
+                                     ref_high = c(99, 25, 59, 47.1))
+  row.names(ibi_ref_ranges_table) <- c("NumOfTaxa", "NumOfMolluscTaxa", "NotomastusAbun", "PctSensTaxa")
+
+
+  ### IBI Metrics:
+  # We stitch together all the necessary IBI metrics to determine the IBI index.
+  # Each of the metrics is then compared to the tables listed above (Table 5.4 and Table 5.5) to determine the IBI score,
+  # the IBI Category, and IBI Category Score
+  ibi_metrics <- ibi1 %>%
+    full_join(ibi2, by = c( "sampledate", "stationid", "replicate")) %>%
+    full_join(ibi3, by = c( "sampledate", "stationid", "replicate")) %>%
+    full_join(ibi4, by = c( "sampledate", "stationid", "replicate"))
+
+  # Export a table of the data classified into IBI metrics for the user to reveiw before they are integrated
+  write.csv(ibi_metrics, file = paste(output_path, "/", file_id, " SQO IBI interim 2 - IBI metric values.csv", sep=""),
+            row.names = FALSE)
+  
+  
+  
+  ### Reference ranges for IBI metrics in Southern California Marine Bays against which sample values are compared
+  ### [ Table 4.19 CASQO Technical Manual 3rd edition 2021 - page 68 ]
+  ibi_ref_ranges_table <- data.frame(metric = c("NumOfTaxa", "NumOfMolluscTaxa", "NotomastusAbun", "PctSensTaxa"),
+                                     ref_low = c(13, 2, 0, 19),
+                                     ref_high = c(99, 25, 59, 47.1))
+ 
+  
+  # Calculate IBI scores
+  ibi.scores<-ibi_metrics %>% 
+    #rearrange data to make scoring cleaner
+    pivot_longer(., cols=c(-stationid, -sampledate, -replicate), names_to = "metric", values_to = "value") %>% 
+    #add in the reference site ranges for comparison
+    left_join(.,ibi_ref_ranges_table, by="metric") %>% 
+    #IBI score starts at 0 and for each metric out of reference rance (< or >), the score increases by 1
+    mutate(out_of_range=case_when(value<ref_low | value>ref_high ~1,
+                                  TRUE~0)) %>% 
+    group_by(stationid, sampledate, replicate) %>% 
+    summarise(score=sum(out_of_range)) %>% 
+    ungroup() %>% 
+    #convert IBI scores to SQO categories and category scores
+    mutate(index="IBI", .before=score) %>% 
+    mutate(condition_category = case_when(score == 0 ~ "Reference", 
+                                          score == 1 ~ "Low Disturbance", 
+                                          score == 2 ~ "Moderate Disturbance", 
+                                          score %in%c(3,4) ~ "High Disturbance"),
+           condition_category_score = case_when(score == 0 ~ 1, 
+                                                score == 1 ~ 2, 
+                                                score == 2 ~ 3, 
+                                                score %in%c(3,4) ~ 4))
+  
+ 
+  #gathering the station information (i.e., non-taxonomic data) for each site
+
+  ibi.stations<-BenthicData %>%
+    select(-taxon, -abundance, -exclude) %>%
+    distinct()
+
+  ibi.out<-ibi.scores %>%
+    select(stationid, sampledate, replicate, index, score, condition_category, condition_category_score) %>%
+    bind_rows(ibi.out.null, ., defaunated) %>%
+    full_join(ibi.stations, ., by=c("stationid", "sampledate", "replicate"))
+
+  if(length(ibi.out$stationid)>1)#if IBI scores are calculated, function will drop the dummy data placeholders and report the data for the submitted samples
+  {
+    ibi.out.2<-ibi.out %>%
+      filter(stationid!="dummy") %>%
+      mutate(note=if_else(is.na(note), "none", note))
+  }
+
+  else
+  {
+    ibi.out.2<-ibi.out %>% # if IBI scores are not calculated, the function will only report the dummy data placeholders
+      mutate(note="IBI scores not caculated")
+  }
+
+
+
+
+ 
+# Export the final scores to the location designated by the output path   
+  write.csv(ibi.out.2, paste(output_path, "/", file_id, " SQO IBI scores.csv", sep=""), row.names = FALSE)
+
+# Export the final scores to the general R environment
+   return(ibi.out.2)
+
+
+}
+
