09694266cc97af4e90928e32ceca98f044bfb294
David Gillett <59022857+DavidGillettSCCWRP@users.noreply.github.com>
2026-03-23 18:03:20 +0000

    re-running the sqo code with the v2.0 of the script

--- R/example index calculation script.R
+++ R/example index calculation script.R
@@ -7,14 +7,14 @@
 
 # this one runs the whole example data
 
-full.bri<-offshore_bri_calc_ed14(file_id="djg test",
+full.bri<-offshore_bri_calc_ed14_v2(file_id="djg test",
                                  infauna_path="example BRI data/example infauna.csv",
                                  station_path="example BRI data/example stations.csv",
                                  output_path = "BRI dry run")
 
 # this one runs the unchanged taxa example data
 
-uct.bri<-offshore_bri_calc_ed14(file_id="djg test - unchanged",
+uct.bri<-offshore_bri_calc_ed14_v2(file_id="djg test - unchanged",
                                  infauna_path="example BRI data/example BRI infauna - unchanged taxa.csv",
                                  station_path="example BRI data/example BRI stations - unchanged taxa.csv",
                                  output_path = "BRI dry run")
@@ -23,13 +23,13 @@ uct.bri<-offshore_bri_calc_ed14(file_id="djg test - unchanged",
 
 # this one runs the whole data example
 
-full.sqo<-alt.SQO_BLOE.generic(file_id = "djg test",
+full.sqo<-SQO_BLOE.generic_v2(file_id = "djg test",
                                infauna_path = "example SQO data/example SQO data.csv",
                                output_path = "SQO dry run")
 
 # this one runs the unchanged taxa example data
 
-uct.sqo<-alt.SQO_BLOE.generic(file_id = "djg test - unchanged",
+uct.sqo<-SQO_BLOE.generic_v2(file_id = "djg test - unchanged",
                                infauna_path = "example SQO data/example SQO data - unchanged taxa.csv",
                                output_path = "SQO dry run")
 
